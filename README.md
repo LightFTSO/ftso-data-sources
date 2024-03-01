51d1c3aed5d41446ec07ceaae7125d6ef1263d31
Jimmy <jgdames@gmail.com>
2024-02-29 23:26:08 -0600

    refactor default config variables to its own file, update readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 -by LightFTSO
 
-Flow goes like this:
+Data flow goes like this:
 
-1. Instantiate a data source (could be crypto exchange, forex api, stocks api, or whatever)
-2.
+1. Configure and create data consumers (file, redis, mqtt, websocket, influxdb, questdb, etc)
+2. Instantiate a (list of) data source(s) (could be crypto exchange, forex api, stocks api, or whatever)
+2. Send parsed trade and/or ticker data to each of the consumers via a broadcast channel
+3. Optionally generate and print statistics in a determined interval of time
+4. ?
+5. profit
---
+++ config/defaults.go
@@ -0,0 +1,37 @@
+package config
+
+import "github.com/spf13/viper"
+
+func setDefaults() {
+	viper.SetDefault("env", "development")
+
+	viper.SetDefault("enabled_streams.trades", false)
+	viper.SetDefault("enabled_streams.tickers", false)
+
+	viper.SetDefault("message_buffer_size", 0)
+
+	viper.SetDefault("stats.enabled", "1h")
+	viper.SetDefault("stats.interval", "1h")
+	viper.SetDefault("stats.num_threads", 1)
+
+	viper.SetDefault("datasources", []string{"noisy"})
+
+	viper.SetDefault("file_consumer.enabled", false)
+	viper.SetDefault("file_consumer.filename", "")
+
+	viper.SetDefault("redis.enabled", false)
+	viper.SetDefault("redis.client_options.initaddress", []string{"127.0.0.1:6379"})
+	viper.SetDefault("redis.client_options.username", "")
+	viper.SetDefault("redis.client_options.password", "")
+	viper.SetDefault("redis.num_threads", 1)
+	viper.SetDefault("redis.include_stdout", false)
+	viper.SetDefault("redis.ts.retention", "24h")
+	viper.SetDefault("redis.ts.chunksize", 4096)
+
+	viper.SetDefault("websocket_server.enabled", false)
+	viper.SetDefault("websocket_server.host", "127.0.0.1")
+	viper.SetDefault("websocket_server.port", 3000)
+	viper.SetDefault("websocket_server.endpoints.trades", "/trades")
+	viper.SetDefault("websocket_server.endpoints.tickers", "/tickers")
+	viper.SetDefault("websocket_server.endpoints.volumes", "/volumes")
+}

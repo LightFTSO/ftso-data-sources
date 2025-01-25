6dccbcafbfc5525ffb44a77a9957d0302e41dcd4
JamesGDiaz <47843895+JamesGDiaz@users.noreply.github.com>
2025-01-25 20:36:34 +0900

    Update symbol.go

--- model/symbol.go
+++ model/symbol.go
@@ -37,27 +37,28 @@ func cleanRemotePair(s string) string {
 
 func getBaseCurrency(pair string) string {
 	for _, stablecoin := range constants.USDT_USDC_DAI {
-		upperSc := strings.ToUpper(stablecoin)
-		if strings.HasPrefix(pair, upperSc) {
-			return upperSc
+		if strings.HasPrefix(pair, stablecoin) {
+			return stablecoin
 		}
 	}
 
-	upperDAI := strings.ToUpper(constants.DAI)
-	if strings.HasSuffix(pair, upperDAI) {
-		return strings.Replace(pair, upperDAI, "", 1)
+	for _, quote := range constants.USD_USDT_USDC_DAI {
+		base, found := strings.CutSuffix(pair, quote)
+		if found {
+			return base
+		}
 	}
 
 	var quote = substr(pair, len(pair)-4, 6)
 
 	if !strings.HasPrefix(quote, "U") /*&& !strings.HasPrefix(quote, "B") */ {
-		quote = "USD"
+		quote = constants.USD
 	}
 
 	var base = strings.Replace(pair, quote, "", 1)
 
 	if len(base) == 2 {
-		quote = "USD"
+		quote = constants.USD
 		base = strings.Replace(pair, quote, "", 1)
 	}
 
@@ -76,19 +77,4 @@ func substr(input string, start int, length int) string {
 	}
 
 	return string(asRunes[start : start+length])
-}
-
-type SymbolList []Symbol
-
-func (s SymbolList) ChunkSymbols(chunkSize int) []SymbolList {
-	var chunks []SymbolList
-	for i := 0; i < len(s); i += chunkSize {
-		end := i + chunkSize
-		if end > len(s) {
-			end = len(s)
-		}
-		chunks = append(chunks, s[i:end])
-	}
-	return chunks
-
 }
